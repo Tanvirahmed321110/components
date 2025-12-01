da582dd2a45f697caa0372274fb4564b9798e808
tanvir321110 <tanvirahmed321110@gmail.com>
2025-12-01 17:23:21 +0600

    sidebar design done

--- static/js/global_js/global.js
+++ static/js/global_js/global.js
@@ -763,6 +763,45 @@ mSidebarDropdwonF()
 
 
 
+//=========  For common dropdwon 2 and Filter head  ===========
+const dropdownParents = document.querySelectorAll(".filter-header .click-dropdown-parent,.click-dropdown-parent.common-dropdwon-2");
+
+if (dropdownParents) {
+    dropdownParents.forEach(parent => {
+        const btn = parent.querySelector(".click-dropdown-btn span:first-child,.text-content"); // button text span
+        const items = parent.querySelectorAll(".click-dropdown .item");
+
+        // Only proceed if items and button exist
+        if (items && btn) {
+            items.forEach(item => {
+                item.addEventListener("click", () => {
+                    // Remove active from all items
+                    items.forEach(i => i.classList.remove("active"));
+
+                    // Add active to clicked item
+                    item.classList.add("active");
+
+                    // remove active class closet dropdown
+                    const dropdown = item.closest('.click-dropdown')
+                    dropdown.classList.remove('active')
+
+                    // Update button text with max 16 chars + "…"
+                    btn.textContent = item.textContent.length > 16
+                        ? item.textContent.slice(0, 16) + "…"
+                        : item.textContent;
+
+                    // ✅ Call commonDropdown2 only if parent has class
+                    if (parent.classList.contains("common-dropdwon-2")) {
+                        commonDropdown2(btn, item);
+                    }
+                });
+            });
+        }
+    });
+}
+
+
+
 
 
 //===========  Customer Tab Section  ===========
