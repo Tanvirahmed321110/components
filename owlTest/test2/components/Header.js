c2d2bbc81f0b4a1ca7cd6ea91cf1361d8e96a298
tanvir321110 <tanvirahmed321110@gmail.com>
2026-03-05 10:40:44 +0600

    recent view page start for global

--- owlTest/test2/components/Header.js
+++
@@ -1,9 +0,0 @@
-import { Component, xml } from "@odoo/owl";
-
-export class Header extends Component {
-    static template = xml`
-        <header style="background:#1e293b;color:white;padding:15px;">
-            <h2>🎓 Student Portal</h2>
-        </header>
-    `;
-}
